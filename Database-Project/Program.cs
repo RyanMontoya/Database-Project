860e187c74553a3bff18aa9157130d280b81d1ed
ryan-montoya <rmonto6@uw.edu>
2023-02-27 01:34:34 -0800

    Add project files.

---
+++ Database-Project/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+class program {
+
+    static void Main(String[] args)
+    {
+
+        program p = new program();
+        String tmp = p.database(); 
+        Console.WriteLine("Hello, World!"+p);
+
+
+    } 
+
+    public String database()
+    {
+        return "database";
+    }
+
+
+   }
